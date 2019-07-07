4f4f492bab7ffc89b8341fdac265cfee59716513
vinodkumar.gaggara <vinodkumar.gaggara@asg.com>
2019-07-08 01:12:39 +0530

    1. View search results 2. Tic tac toe game added

--- weather/src/component/weather/weather.js
+++ weather/src/component/weather/weather.js
@@ -1,57 +1,29 @@
 import React from 'react';
 import "./weather.css";
-import WebServices from './../../services/webservices';
+import WeatherSearch from '../weathersearch/weathersearch';
+import WeatherResults from '../weatherresults/weatherresults';
+
+
+
 class Weather extends React.Component{
+    state = {
+        "result" : {}
+    }
+
+    updateResults = (results) => {
+        this.setState({
+            "result" : results
+        });
+        console.log("Updated results - " + results);
+    }
+
     render() {
         return (
             <div className="weather-body">
-                <WeatherSearch />
+                <WeatherSearch result={this.updateResults}/>
+                <WeatherResults result={this.state.result}/>
             </div>
         );
     }
 }
-
-class WeatherSearch extends React.Component {
-    
-    get cityName(){
-        return this._cityName;
-    }
-
-    set cityName(value){
-        this._cityName = value;
-    }
-
-    state = {
-        "cityName" : "Hyderabad"
-    }
-
-    render() {
-        return (
-            <div className="search-city">
-                <div className="input-group">
-                    <input type="text" className="custom-input" placeholder="City Name" value={this.state.cityName} onChange={this.updateCityName}/>
-                    <div className="input-group-append">
-                        <button className="btn btn-outline-secondary" type="button" onClick={() => this.getWeatherDetails()}>Button</button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-
-    updateCityName = (event) => {
-        this.setState({
-            "cityName" : event.target.value
-        });
-    }
-
-    getWeatherDetails(){
-        const webServices = new WebServices();
-        let url = "http://api.openweathermap.org/data/2.5/weather?q=" + this.state.cityName + "&APPID=5536f021b58776944ade38cabdf28b46";
-        let fetchRequest = webServices.get(url, {});
-        fetchRequest.then((response) => {
-            console.log(response);
-        });
-    }
-}
-
 export default Weather;
