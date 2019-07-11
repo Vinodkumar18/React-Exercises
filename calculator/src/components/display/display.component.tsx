1b769ff4ace8071dfe29982026b1ef1eba02331f
vinodkumar.gaggara <vinodkumar.gaggara@asg.com>
2019-07-12 00:10:39 +0530

    1. Functionality app 2. Removed unnessary files
    TODO :
    1. Unit testing
    2. Height fix to full viewport

--- calculator/src/components/display/display.component.tsx
+++ calculator/src/components/display/display.component.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface Inputs {
-    "value" : number
+    "value" : string
 }
 
 export class Display extends React.Component<Inputs> {
--- calculator/src/components/keyboard/operations/operations.component.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export class Operations extends React.Component {
-    render() {
-        return (
-            <div>Operations</div>
-        );
-    }
-}
